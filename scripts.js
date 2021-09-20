5632a49a20a85c549a98fe8fd0a15e8aedc976a1
OmarJ <omarjandali93@gmail.com>
2021-09-20 00:37:52 -0700

    displayed moods with button using javascript

---
+++ scripts.js
@@ -0,0 +1,30 @@
+import data from './data.js';
+
+const itemsContainer = document.querySelector('#items')
+
+for (let i = 0; i < data.length; i++) {
+	// create a new div element and give it a class name
+	const newDiv = document.createElement('div');
+	newDiv.className = 'item'
+	// create an image element
+    const img = document.createElement('img');
+    const des = document.createElement('p');
+    const price = document.createElement('p');
+    const button = document.createElement('button')
+	// this will change each time we go through the loop. Can you explain why?
+	img.src = data[i].image
+	img.width = 300
+    img.height = 300
+    des.innerHTML = data[i].desc
+    price.innerHTML = data[i].price
+    button.id = data[i].name
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+	// Add the image to the div
+    newDiv.appendChild(img)
+    newDiv.appendChild(des)
+    newDiv.appendChild(price)
+    newDiv.appendChild(button)
+	console.log(img) // Check the console!
+	itemsContainer.appendChild(newDiv)
+}
